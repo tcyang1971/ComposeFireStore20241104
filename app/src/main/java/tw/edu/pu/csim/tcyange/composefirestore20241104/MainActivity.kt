73ca73e85fa7ce9b3841ae94b933ba65ff4ec310
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-11-04 15:01:02 +0800

    v1_引用及ComposableFunction

--- app/src/main/java/tw/edu/pu/csim/tcyange/composefirestore20241104/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyange/composefirestore20241104/MainActivity.kt
@@ -13,6 +13,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.tcyange.composefirestore20241104.ui.theme.ComposeFireStore20241104Theme
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.material3.Button
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +35,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeFireStore20241104Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    /*
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
+
+                     */
+                    Birth(m = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +50,6 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Birth(m: Modifier){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeFireStore20241104Theme {
-        Greeting("Android")
-    }
 }
