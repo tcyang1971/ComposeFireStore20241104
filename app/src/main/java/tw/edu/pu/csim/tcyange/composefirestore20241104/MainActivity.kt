147e93a08127f9b5b3094cf719dc1b8307064254
tcyang1971 <tcyang@gm.pu.edu.tw>
2024-11-04 15:29:31 +0800

    v3_出生體重輸入

--- app/src/main/java/tw/edu/pu/csim/tcyange/composefirestore20241104/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyange/composefirestore20241104/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Button
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.text.input.KeyboardType
 
 
 class MainActivity : ComponentActivity() {
@@ -52,6 +53,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("楊子青")}
+    var userWeight by remember { mutableStateOf(3800)}
 
     Column {
         TextField(
@@ -65,7 +67,21 @@ fun Birth(m: Modifier){
 
         )
 
-        Text("您輸入的姓名是：$userName")
+        TextField(
+            value = userWeight.toString(),
+            onValueChange = { newText ->
+                if (newText == ""){
+                    userWeight = 0
+                }
+                else{userWeight = newText.toInt()}
+            },
+            label = { Text("出生體重") },
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Number)
+        )
+
+
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
     }
 
 }
